a789b1633fc43a672fe4130d18321352f7cfc279
Tathagat017 <114250830+Tathagat017@users.noreply.github.com>
2025-07-25 21:43:35 +0530

    temp conversion

---
+++ temprature_conversion_functions.py
@@ -0,0 +1,11 @@
+def celcius_to_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+
+def fahrenheit_to_celcius(fahrenheit):
+    return (fahrenheit - 32) * 5/9
+
+def kelvin_to_celcius(kelvin):
+    return kelvin - 273.15
+
+def farenheit_to_kelvin(fahrenheit):
+    return (fahrenheit - 32) * 5/9 + 273.15
